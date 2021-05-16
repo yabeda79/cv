b9151589f68b8b597c57abe9ada6d229d6c1f9d3
Vladislav Karol <vlad230899@gmail.com>
2021-05-16 20:00:34 +0300

    added mui grid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <h1>hello</h1>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+          <Paper className={classes.paper}>photo</Paper>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+          <Paper className={classes.paper}>text</Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 };
