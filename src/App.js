81e9258c381abb1349038e04e9db739bfb030291
Vladislav Karol <vlad230899@gmail.com>
2021-05-24 13:54:31 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import photo from "./img/cv_photo.jpg";
 
 //MUI
@@ -12,7 +12,7 @@ import Container from "@material-ui/core/Container";
 
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import EmailIcon from "@material-ui/icons/Email";
+// import EmailIcon from "@material-ui/icons/Email";
 import TelegramIcon from "@material-ui/icons/Telegram";
 
 //import components
@@ -22,17 +22,7 @@ import Exp from "./components/Exp";
 import Education from "./components/Education";
 import Skills from "./components/Skills";
 import Lang from "./components/Lang";
-
-/*
-things to add:
-1. theme swicher
-2. contact links (absolute position at the bottom side of photo with text like "hello im vladislav karol")
-
-questions:
-1. fonts awesome connection (or MUI icons)
-2. MUI theme 
-3. img positioning
-*/
+import Contacts from "./components/Contacts";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -41,22 +31,13 @@ const useStyles = makeStyles((theme) => ({
   photo: {
     height: "100vh",
     width: "auto",
-    // margin: "0 auto",
   },
   img_container: {
     overflow: "hidden",
     justifyContent: "center",
     alignContent: "center",
     display: "flex",
-    // justifyContent: "center",
-    // objectPosition: "center",
   },
-  // absolute: {
-  //   position: "absolute",
-  //   top: "50%",
-  //   left: "50%",
-  //   transform: "translate(-50%, -50%)",
-  // },
   fixed: {
     position: "fixed",
     top: "0",
@@ -70,6 +51,26 @@ const useStyles = makeStyles((theme) => ({
   smallvw: {
     width: "100vw",
   },
+  img_fixed: {
+    position: "fixed",
+    width: "50vw",
+    overflow: "hidden",
+    justifyContent: "center",
+    alignContent: "center",
+    display: "flex",
+  },
+  mobile_font_size_hi: {
+    fontSize: "32px",
+  },
+  desktop_font_size_hi: {
+    fontSize: "50px",
+  },
+  mobile_font_size_me: {
+    fontSize: "24px",
+  },
+  desktop_font_size_me: {
+    fontSize: "32px",
+  },
 }));
 
 const StyledEmptyDiv = styled(Typography)({
@@ -77,27 +78,28 @@ const StyledEmptyDiv = styled(Typography)({
 });
 
 const StyledHelloBold = styled(Typography)({
-  fontSize: "50px",
   fontWeight: "bold",
   color: "#bebece",
+  textAlign: "center",
+  justifySelf: "center",
 });
+
+// const StyledMeBold = styled(Typography)({
+//   fontWeight: "bold",
+//   color: "#bebece",
+//   textAlign: "center",
+//   justifySelf: "center",
+// });
 
 const StyledTranspDiv = styled(Container)({
   position: "absolute",
   left: "0",
   bottom: "0",
-  // width: "50vw",
-  // width: window.screen.width < 960 ? "50vw" : "100vw",
   height: "100vh",
 });
 
-// if(window.screen.width<960){width: "50vw"} else {width: "100vw"}
-// (window.screen.width<960) ? {width: "50vw"} : {width: "100vw"}
-
-// const StyledTranspDiv = styled("div")``;
-
 const StyledIconsDiv = styled("div")({
-  width: "50%",
+  width: "35%", //50%
   position: "absolute",
   left: "50%",
   bottom: "5%",
@@ -110,11 +112,21 @@ const StyledHelloDiv = styled("div")({
   width: "80%",
   position: "absolute",
   left: "50%",
-  bottom: "10%",
+  bottom: "10%", // 15%
   transform: "translate(-50%, -50%)",
   display: "flex",
   justifyContent: "center",
 });
+
+// const StyledMeDiv = styled("div")({
+//   width: "80%",
+//   position: "absolute",
+//   left: "50%",
+//   bottom: "11%",
+//   transform: "translate(-50%, -50%)",
+//   display: "flex",
+//   justifyContent: "center",
+// });
 
 const StyledGradientDiv = styled("div")({
   width: "100%",
@@ -122,7 +134,6 @@ const StyledGradientDiv = styled("div")({
   position: "absolute",
   left: "0%",
   bottom: "0%",
-  // background: "rgb(34,54,195)",
   background:
     "linear-gradient(0deg, rgba(34,54,195,1) 0%, rgba(255,255,255,0) 100%)",
 });
@@ -136,21 +147,10 @@ const App = () => {
     setVw(window.screen.width);
   };
 
-  // useEffect(() => {
-  //   init();
-  // }, []);
-
   window.addEventListener("resize", init);
-
-  console.log(window.screen.width);
 
   return (
     <div className={appClasses.root}>
-      {/* <img
-        src={photo}
-        alt="my_photo"
-        className={(appClasses.photo, appClasses.fixed)}
-      /> */}
       <Grid container spacing={3}>
         <Grid
           className={appClasses.img_container}
@@ -161,24 +161,62 @@ const App = () => {
           lg={6}
           xl={6}
         >
-          <img src={photo} alt="my_photo" className={appClasses.photo} />
-          <StyledTranspDiv
-            // style={
-            //   window.screen.width < 960 ? { width: "50vw" } : { width: "100vw" }
-            // }
-            className={vw < 960 ? appClasses.smallvw : appClasses.largevw}
-          >
-            <StyledGradientDiv></StyledGradientDiv>
-            <StyledHelloDiv>
-              <StyledHelloBold>Hi, I'm Vladislav Karol</StyledHelloBold>
-            </StyledHelloDiv>
-            <StyledIconsDiv>
-              <GitHubIcon fontSize="large" style={{ color: "#bebece" }} />
-              <LinkedInIcon fontSize="large" style={{ color: "#bebece" }} />
-              <EmailIcon fontSize="large" style={{ color: "#bebece" }} />
-              <TelegramIcon fontSize="large" style={{ color: "#bebece" }} />
-            </StyledIconsDiv>
-          </StyledTranspDiv>
+          <div className={vw < 960 ? "" : appClasses.img_fixed}>
+            <img src={photo} alt="my_photo" className={appClasses.photo} />
+            <StyledTranspDiv
+              className={vw < 960 ? appClasses.smallvw : appClasses.largevw}
+            >
+              <StyledGradientDiv></StyledGradientDiv>
+              <StyledHelloDiv>
+                <StyledHelloBold
+                  className={
+                    vw < 960
+                      ? appClasses.mobile_font_size_hi
+                      : appClasses.desktop_font_size_hi
+                  }
+                >
+                  Hi, I'm Vladislav Karol
+                </StyledHelloBold>
+              </StyledHelloDiv>
+              {/* <StyledMeDiv>
+                <StyledMeBold
+                  className={
+                    vw < 960
+                      ? appClasses.mobile_font_size_me
+                      : appClasses.desktop_font_size_me
+                  }
+                >
+                  A beginner front-end developer
+                </StyledMeBold>
+              </StyledMeDiv> */}
+              <StyledIconsDiv>
+                <a
+                  href="http://github.com/yabeda79/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <GitHubIcon fontSize="large" style={{ color: "#bebece" }} />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/vladislav-karol-0303891ba/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <LinkedInIcon fontSize="large" style={{ color: "#bebece" }} />
+                </a>
+                {/* <a href="http://t.me/vladislavkarol" target="_blank">
+                  <EmailIcon fontSize="large" style={{ color: "#bebece" }} />
+                </a> */}
+                <a
+                  href="http://t.me/vladislavkarol"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <TelegramIcon fontSize="large" style={{ color: "#bebece" }} />
+                </a>
+              </StyledIconsDiv>
+            </StyledTranspDiv>
+          </div>
         </Grid>
         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
           <About />
@@ -190,6 +228,8 @@ const App = () => {
           <Skills />
           <StyledEmptyDiv />
           <Lang />
+          <StyledEmptyDiv />
+          <Contacts />
           <StyledEmptyDiv />
         </Grid>
       </Grid>
---
+++ src/components/Contacts.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { styled } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+
+/*
+contacts info
+*/
+
+const StyledHeaderBold = styled(Typography)({
+  fontSize: "24px",
+  fontWeight: "bold",
+  color: "#bebece",
+  padding: "10px",
+});
+
+const StyledConHeader = styled(Typography)({
+  fontSize: "15px",
+  fontWeight: "bold",
+  color: "#bebece",
+  paddingLeft: "10px",
+});
+
+const StyledParagraphMain = styled(Typography)({
+  fontSize: "15px",
+  color: "#3d3d50",
+  paddingLeft: "10px",
+});
+
+const Contacts = () => {
+  return (
+    <div>
+      <StyledHeaderBold>Contacts</StyledHeaderBold>
+      <Grid container spacing={3}>
+        <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
+          <StyledConHeader>Phone:</StyledConHeader>
+          <StyledParagraphMain>+375 29 550-23-81</StyledParagraphMain>
+        </Grid>
+        <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
+          <StyledConHeader>Email:</StyledConHeader>
+          <StyledParagraphMain>+375 29 550-23-81</StyledParagraphMain>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Contacts;
